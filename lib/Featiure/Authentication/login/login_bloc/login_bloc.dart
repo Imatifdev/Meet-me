9859af370d413bb7dfeda6607ddc467f6d3cc276
Muhammad Atif Pervaiz <matifpervaizofficial@gmail.com>
2023-08-14 11:26:33 +0500

    google auth

--- lib/Featiure/Authentication/login/login_bloc/login_bloc.dart
+++ lib/Featiure/Authentication/login/login_bloc/login_bloc.dart
@@ -2,75 +2,34 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meetly/Featiure/Authentication/login/login_bloc/login_event.dart';
 import 'package:meetly/Featiure/Authentication/login/login_bloc/login_states.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  AuthBloc() : super(AuthInitialState()){
-    
-    on<LoginEvent>((event, emit) async{
-        try{
-          UserCredential authResult = await _auth.signInWithEmailAndPassword(
+  AuthBloc() : super(AuthInitialState()) {
+    on<LoginEvent>((event, emit) async {
+      try {
+        UserCredential authResult = await _auth.signInWithEmailAndPassword(
           email: event.email,
           password: event.password,
         );
         emit(AuthLoggedInState(authResult.user!));
-        } catch(e){
-          emit(AuthErrorState(e.toString()));
-        }
+      } catch (e) {
+        emit(AuthErrorState(e.toString()));
+      }
     });
 
-    on<SignUpEvent>((event, emit) async{
+    on<SignUpEvent>((event, emit) async {
       try {
-      UserCredential authResult = await _auth.createUserWithEmailAndPassword(
-        email: event.email,
-        password: event.password,
-      );
-      emit(AuthLoggedInState(authResult.user!));
-    } catch (e) {
-      emit(AuthErrorState(e.toString()));
-    }
+        UserCredential authResult = await _auth.createUserWithEmailAndPassword(
+          email: event.email,
+          password: event.password,
+        );
+        emit(AuthLoggedInState(authResult.user!));
+      } catch (e) {
+        emit(AuthErrorState(e.toString()));
+      }
     });
   }
-
- 
-
-
-  // // Stream<AuthState> mapEventToState(AuthEvent event) async* {
-    
-    
-  // //   if (event is LoginEvent) {
-  // //     on<LoginEvent>((event, emit) {});
-  // //     try {
-  // //       UserCredential authResult = await _auth.signInWithEmailAndPassword(
-  // //         email: event.email,
-  // //         password: event.password,
-  // //       );
-        
-  // //       //yield AuthLoggedInState(authResult.user!);
-  // //     } catch (e) {
-  // //       yield AuthErrorState(e.toString());
-  // //     }
-  // //   } else if (event is SignUpEvent) {
-  // //     yield AuthLoadingState();
-  // //     try {
-  // //       UserCredential authResult = await _auth.createUserWithEmailAndPassword(
-  // //         email: event.email,
-  // //         password: event.password,
-  // //       );
-  // //       yield AuthLoggedInState(authResult.user!);
-  // //     } catch (e) {
-  // //       yield AuthErrorState(e.toString());
-  // //     }
-  // //   } else if (event is LogoutEvent) {
-  // //     yield AuthLoadingState();
-  // //     try {
-  // //       await _auth.signOut();
-  // //       yield AuthInitialState();
-  // //     } catch (e) {
-  // //       yield AuthErrorState(e.toString());
-  // //     }
-  // //   }
-  // }
 }
---
+++ lib/google.dart/google.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:meetly/Featiure/home/ui/pages/homepage.dart';
+
+class GoogleAuthWidget extends StatelessWidget {
+  const GoogleAuthWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () async {
+          try {
+            final googleSignIn = GoogleSignIn();
+            final googleUser = await googleSignIn.signIn();
+            final googleAuth = await googleUser!.authentication;
+
+            final credential = GoogleAuthProvider.credential(
+              accessToken: googleAuth.accessToken,
+              idToken: googleAuth.idToken,
+            );
+
+            final userCredential =
+                await FirebaseAuth.instance.signInWithCredential(credential);
+
+            final user = userCredential.user;
+            final displayName = user!.displayName;
+            final email = user.email;
+            final phone = user.phoneNumber;
+            //ssave data in firestore
+            FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+              'First Name': displayName,
+              'Email': email,
+              'Phone': phone
+              // Add more fields as needed
+            });
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (ctx) => HomePage()));
+          } catch (e) {
+            if (e is FirebaseAuthException) {
+              if (e.code == 'account-exists-with-different-credential') {
+                print('An account with the same email already exists');
+              } else {
+                print('Failed to sign up with Google: ${e.message}');
+              }
+            } else {
+              print('Failed to sign up with Google: $e');
+            }
+          }
+        },
+        child: Image(
+            fit: BoxFit.cover,
+            height: 60,
+            width: 60,
+            image: AssetImage('assets/images/logo.png')),
+      ),
+    );
+  }
+}
